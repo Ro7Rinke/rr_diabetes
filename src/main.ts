73159b5e2017f645fb4b23e2e10842d5ee79299b
Rocetti Rinke <ro7rinke@gmail.com>
2025-10-01 19:08:51 -0300

    Habilitado cors e instalado bcrypt

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,13 @@ async function bootstrap() {
     transform: true,
   }));
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
